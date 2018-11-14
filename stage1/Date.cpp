3a66b754698c743e7b24e2eb9d9f3a3e0a771d55
gentleking <1055482958@qq.com>
2018-11-15 01:02:33 +0800

    change function stringToDate

--- stage1/Date.cpp
+++ stage1/Date.cpp
@@ -128,11 +128,11 @@ Date Date::stringToDate(std::string dateString) {
     }
     cout << year << " " << month << " " << day << " " << hour << " " << minute << endl;
     year_ = year;
-	month_ = month;
-	day_ = day;
-	hour_ = hour;
-	minute_ = minute;
-	return *this;
+    month_ = month;
+    day_ = day;
+    hour_ = hour;
+    minute_ = minute;
+    return *this;
 }
 
 std::string Date::dateToString(Date date) {
